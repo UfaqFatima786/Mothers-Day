6bdd886e828525e91e264fb89092e85c68df3f0f
UfaqFatima786 <u.capricorn19@gmail.com>
2026-05-10 03:58:38 +0500

    mother's day

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ cards.forEach((card) => {
         card.style.background = "white";
     });
 });
+
+// Optional Hover Animation Console Message
+
+const galleryCards = document.querySelectorAll(".gallery-card");
+
+galleryCards.forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+        console.log("Image Hovered");
+    });
+});
+
+
+const quoteCards = document.querySelectorAll(".quote-card");
+
+quoteCards.forEach((card) => {
+
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "scale(1.05)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "scale(1)";
+    });
+
+});
+
+// Optional Animation
+console.log("Thank You Section Loaded 💖");
